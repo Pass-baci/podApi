d1cedfffe5e90e6144c770b9ae579d57de7c2763
v_chgvchen <v_chgvchen@tencent.com>
2023-01-03 15:21:40 +0800

    edit hystrix

--- plugin/hystrix/hystrix.go
+++ plugin/hystrix/hystrix.go
@@ -5,26 +5,27 @@ import (
 	"fmt"
 	"github.com/afex/hystrix-go/hystrix"
 	"github.com/asim/go-micro/v3/client"
-	"github.com/asim/go-micro/v3/logger"
 )
 
 type clientWrapper struct {
 	client.Client
 }
 
-// Call 熔断逻辑
+//熔断逻辑
 func (c *clientWrapper) Call(ctx context.Context, req client.Request, rsp interface{}, opts ...client.CallOption) error {
 	return hystrix.Do(req.Service()+"."+req.Endpoint(), func() error {
-		logger.Info(req.Service() + "." + req.Endpoint())
+		//正常执行
+		fmt.Println(req.Service() + "." + req.Endpoint())
 		return c.Client.Call(ctx, req, rsp, opts...)
 	}, func(e error) error {
+		//走熔断逻辑,每个服务都不一样
 		fmt.Println(req.Service() + "." + req.Endpoint() + "的熔断逻辑")
 		return e
 	})
 }
 
 func NewClientHystrixWrapper() client.Wrapper {
-	return func(c client.Client) client.Client {
-		return &clientWrapper{}
+	return func(i client.Client) client.Client {
+		return &clientWrapper{i}
 	}
 }
